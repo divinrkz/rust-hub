840967940286a908c74acfa63a17fddb56eaa92a
= <=>
2021-08-13 14:49:25 +0200

    (chore): Hello world rust

--- hello-rust/target/rls/debug/deps/hello_rust-e0c55ac690078482.d
+++
@@ -1,5 +0,0 @@
-/home/irakizadivin/dev/rust-hub/hello-rust/target/rls/debug/deps/hello_rust-e0c55ac690078482.rmeta: src/main.rs
-
-/home/irakizadivin/dev/rust-hub/hello-rust/target/rls/debug/deps/hello_rust-e0c55ac690078482.d: src/main.rs
-
-src/main.rs:
--- hello-rust/target/rls/debug/deps/hello_rust-e866340884e0dfb7.d
+++
@@ -1,5 +0,0 @@
-/home/irakizadivin/dev/rust-hub/hello-rust/target/rls/debug/deps/hello_rust-e866340884e0dfb7.rmeta: src/main.rs
-
-/home/irakizadivin/dev/rust-hub/hello-rust/target/rls/debug/deps/hello_rust-e866340884e0dfb7.d: src/main.rs
-
-src/main.rs:
