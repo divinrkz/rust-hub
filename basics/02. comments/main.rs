39f36c7a0adfb2d350752f6b49b8f22dbd5b290e
= <=>
2021-08-16 07:11:14 +0200

    (chore): added comments sections

---
+++ basics/02. comments/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // println!("Hello, world!");
+
+
+    /* 
+     * This is another type of comment, a block comment.
+     */
+
+    /*
+    Note: The previous column of `*` was entirely for style. There's
+    no actual need for it.
+    */
+
+    //! Generate library docs for the enclosing item.
+
+    println!("This is an example of a comment");
+}
