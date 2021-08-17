d5d9adbd946049e832c54b48fa74a6610ee40a8a
= <=>
2021-08-17 09:53:27 +0200

    (chore): added for flow controls examples.

--- basics/09. flow-of-control/04. for-and-range/main.rs
+++ basics/09. flow-of-control/04. for-and-range/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     // `n` will takes the values: 1, 2 ,,, 100 
-    for n in 1 .. 101 {
+    for n in 1..101 {
         if n % 15 == 0 {
             println!("fizzbuzz");
         } else if n % 3 == 0 {
@@ -9,7 +9,52 @@ fn main() {
             println!("buzz");
         } else {
             println!("{}", n);
-
         }
     }
+
+    // Alternatively, a..=b can be used for a range that is inclusive on both ends. The above can be written as:
+    // for n in 1..=100
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+    
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("I am a rustacean."),
+            _ => println!("Hello {}", name),
+            //
+        }
+        println!("names: {:?}", names);
+    }
+
+
+
+
+    fn main() {
+        let names = vec!["Bob", "Frank", "Ferris"];
+    
+        for name in names.into_iter() {
+            match name {
+                "Ferris" => println!("There is a rustacean among us!"),
+                _ => println!("Hello {}", name),
+            }
+        }
+        
+        // println!("names: {:?}", names);
+        // FIXME ^ Comment out this line
+    }
+
+
+    // into_iter - This consumes the collection so that on each iteration the exact data is provided. Once the collection has been consumed it is no longer available for reuse as it has been 'moved' within the loop.
+    // iter_mut - This mutably borrows each element of the collection, allowing for the collection to be modified in place.
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names);
 }
