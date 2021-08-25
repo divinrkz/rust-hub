b9b52b8ddd15cd234c1e07a0d766c6987523cc8b
divinirakiza <irakizadivin@gmail.com>
2021-08-25 09:47:40 +0200

    (chore): crate examples.

--- basics/12. crates/main.rs
+++ basics/12. crates/main.rs
@@ -6,3 +6,20 @@
 //  before running the compiler over it. 
 // In other words, modules do not get compiled individually,
 //  only crates get compiled.
+
+
+// To link a crate to this new library you may use rustc's --extern flag.
+//  All of its items will then be imported under a module named the same as the library.
+//  This module generally behaves the same way as any other module.
+
+
+// extern crate rary; // May be required for Rust 2015 edition or earlier
+
+fn main() {
+    rary::public_function();
+
+    // Error! `private_function` is private
+    //rary::private_function();
+
+    rary::indirect_access();
+}
