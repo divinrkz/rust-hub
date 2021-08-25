8ff8a769a161a6df0db094c2bf62a8232529420f
divinirakiza <irakizadivin@gmail.com>
2021-08-25 20:50:39 +0200

    (chore): cargo docs

---
+++ basics/13. Cargo/main.rs
@@ -0,0 +1,9 @@
+// cargo is the official Rust package management tool. 
+// It has lots of really useful features to improve code quality and developer velocity!
+// or example, suppose that we want our program to have a great CLI. 
+// You can find lots of great packages on crates.io (the official Rust package registry). 
+// One popular choice is clap. As of this writing, 
+// the most recent published version of clap is 2.27.1. 
+// To add a dependency to our program,
+//  we can simply add the following to our Cargo.toml under [dependencies]: clap = "2.27.1". And that's it! You can start using clap in your program.
+ 
