d597e838390b40fd6c5aa651e9ee9b8aaf6d6bdf
divinirakiza <irakizadivin@gmail.com>
2021-08-27 09:10:41 +0200

    (chore): added cargo docs

--- basics/13. Cargo/main.rs
+++ basics/13. Cargo/main.rs
@@ -4,6 +4,14 @@
 // You can find lots of great packages on crates.io (the official Rust package registry). 
 // One popular choice is clap. As of this writing, 
 // the most recent published version of clap is 2.27.1. 
+
+
 // To add a dependency to our program,
 //  we can simply add the following to our Cargo.toml under [dependencies]: clap = "2.27.1". And that's it! You can start using clap in your program.
  
+// To build our project we can execute cargo build anywhere in the project directory
+//  (including subdirectories!). We can also do cargo run to build and run.
+//  Notice that these commands will resolve all dependencies, download crates if needed,
+//  and build everything, including your crate.
+//  (Note that it only rebuilds what it has not already built, similar to make).
+
