0caf65e852570b0539914354f19328e1eb479421
divinirakiza <irakizadivin@gmail.com>
2021-08-27 09:20:56 +0200

    (chore): Rust attributes explainted.

---
+++ basics/14. attributes/01. dead_code/main.rs
@@ -0,0 +1,16 @@
+// dead_code
+// The compiler provides a dead_code lint that will warn about unused functions.
+//   An attribute can be used to disable the lint.
+
+fn used_function() {}
+
+// `#[allow(dead_code)]` is an attribute that disables the `dead_code` lint
+#[allow(dead_code)]
+fn unused_function() {}
+
+fn noisy_unused_function() {}
+// FIXME ^ Add an attribute to suppress the warning
+
+fn main() {
+    used_function();
+}
