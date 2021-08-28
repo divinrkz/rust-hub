2aa9d8aed086ff1901f78412803baa13709bec3d
divinirakiza <irakizadivin@gmail.com>
2021-08-28 18:45:38 +0200

    (chore): Generics Implementation parts.

--- basics/15. generics/02. implementation/main.rs
+++ basics/15. generics/02. implementation/main.rs
@@ -10,3 +10,33 @@ impl GenericVal<S> {} // Specify `S` as defined above
 // `<T>` Must precede the type to remain generic
 impl<T> GenericVal<T> {}
 
+
+
+struct Val {
+    val: f64,
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+// impl of Val
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+// impl of GenVal for a generic type `T`
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
+
+fn main() {
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{}, {}", x.value(), y.value());
+}
