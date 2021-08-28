53c774fa4f370e42eb5d4126922900ea57bc274a
divinirakiza <irakizadivin@gmail.com>
2021-08-28 18:43:22 +0200

    (chore): Implementation of generics/

---
+++ basics/15. generics/02. implementation/main.rs
@@ -0,0 +1,12 @@
+// Similar to functions, implementations require care to remain generic.
+
+struct S; // Concrete type `S`
+struct GenericVal<T>(T); // Generic type `GenericVal`
+
+// impl of GenericVal where we explicitly specify type parameters:
+impl GenericVal<f32> {} // Specify `f32`
+impl GenericVal<S> {} // Specify `S` as defined above
+
+// `<T>` Must precede the type to remain generic
+impl<T> GenericVal<T> {}
+
