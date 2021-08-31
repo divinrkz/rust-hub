ab9d0534fdcd93575b6d3c6860d340d7ddfc0f53
divinirakiza <irakizadivin@gmail.com>
2021-08-31 11:11:28 +0200

    (chore): traits basic example.

---
+++ basics/16. scoping-rules/main.rs
@@ -0,0 +1,48 @@
+// Scopes play an important part in ownership, borrowing, and lifetimes.
+// That is, they indicate to the compiler when borrows are valid,
+// when resources can be freed, and when variables are created or destroyed.
+
+// raii.rs
+fn create_box() {
+    // Allocate an integer on the heap
+    let _box1 = Box::new(3i32);
+
+    // `_box1` is destroyed here, and memory gets freed
+}
+
+fn main() {
+    // Allocate an integer on the heap
+    let _box2 = Box::new(5i32);
+
+    // A nested scope:
+    {
+        // Allocate an integer on the heap
+        let _box3 = Box::new(4i32);
+
+        // `_box3` is destroyed here, and memory gets freed
+    }
+
+    // Creating lots of boxes just for fun
+    // There's no need to manually free memory!
+    for _ in 0u32..1_000 {
+        create_box();
+    }
+
+    // `_box2` is destroyed here, and memory gets freed
+}
+
+
+
+
+struct ToDrop;
+
+impl Drop for ToDrop {
+    fn drop(&mut self) {
+        println!("ToDrop is being dropped");
+    }
+}
+
+fn main() {
+    let x = ToDrop;
+    println!("Made a ToDrop!");
+}
